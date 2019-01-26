64ed7901ab6d43e5139a64f4cc357702e96ce12a
Rodrigo Sousa <rod_sousa@hotmail.com>
2019-01-26 15:42:25 +0000

    Third Lesson: https://www.youtube.com/watch?v=FV1Ugv1Temg&index=3&list=PL55RiY5tL51q4D-B63KBnygU6opNPFk_q

---
+++ api/routes/products.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET resquest to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST resquest to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You found the secret ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed a simple ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+module.exports = router;
